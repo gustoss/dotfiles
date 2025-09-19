69925bd84c3606be19f14cb432932bf2305255fc
Augusto <augusto.simoes@mercadolivre.com>
2025-09-19 18:53:04 -0300

    More struct to zsh file

---
+++ dot_config/zsh/completions.zsh
@@ -0,0 +1,12 @@
+
+fpath=($ZDOTDIR/completions $fpath)
+
+# Autocompletion
+autoload -Uz compinit
+compinit
+
+# Options
+# setopt GLOB_COMPLETE      # Show autocompletion menu with globs
+# setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
+# setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+# setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
